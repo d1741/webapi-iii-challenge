3440d1358924557a7dd146297ac502a89fd5bec5
d1741 <traciecodes@gmail.com>
2019-09-23 16:55:27 -0500

    Set up put, delete, and created validateUserId middleware in userRouter.js

--- users/userRouter.js
+++ users/userRouter.js
@@ -21,26 +21,79 @@ router.get('/', (req, res) => {
         })
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', validateUserId, (req, res) => {
+    const { id } = req.params;
+    res.status(200).json(req.user);
+    // User.getById(id)
+    //     .then(user => {
+    //         if (user) {
+    //             res.status(200).json(user);
+    //         } else {
+    //             res.status(404).json({ message: "User ID not found"})
+    //         }
+    //     })
+    //     .catch(err => {
+    //         console.log(err);
+    //         res.status(500).json({ error: "Could not get user by ID" })
+    //     })
 });
 
-router.get('/:id/posts', (req, res) => {
-
+router.get('/:id/posts', validateUserId, (req, res) => {
+    const { id } = req.params;
+    User.getUserPosts(id)
+        .then(posts => {
+            res.status(200).json(posts)
+        })
+        .catch(err => {
+            console.log(err + " Check your .get '/:id/posts");
+            res.status(500).json({ error: "Couldn't get user posts" })
+        })
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', validateUserId, (req, res) => {
+    const { id } = req.user;
+    User.remove(id)
+        .then(() => {
+            res.status(204).end()
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({ message: "Delete user didn't work" })
+        })
 });
 
-router.put('/:id', (req, res) => {
-
+router.put('/:id', validateUserId, (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    User.update(id, { name })
+        .then(() => {
+            //res.status(200.json({...req.body, name }))
+            User.getById(id)
+                .then(user => res.status(200).json(user))
+                .catch(err => {
+                    console.log("error in put /:id " + err);
+                    res.status(500).json({error: "error in put /:id update" })
+                })
+        }) 
+        .catch(err => {
+            console.log("error in put /:id " + err);
+            res.status(500).json({error: "error updating user" })
+        })
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-
+    const { id } = req.params;
+    User.getById(id)
+        .then(user => {
+            if (user) {
+                req.user = user;
+                next();
+            } else {
+                res.status(404).json({ message: "User with that ID does not exist" })
+            }
+        })
 };
 
 function validateUser(req, res, next) {
